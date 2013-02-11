601088899bead68284c61861f4798c7367542206
Romain Giot <giot.romain@gmail.com>
2013-02-11 22:29:44 +0100

    Add an extra kind of tests which allows to track the parenthesis bugs

---
+++ equiv/indir.asm
@@ -0,0 +1,9 @@
+; Must be assembled in the same way
+    ld bc, (10+2)*3
+    ld bc, (10+2)*3
+
+; Must be assembled in the same way
+    ld bc, (10+2)*3
+    ld bc, 3*(10+2)
+
+
--- vasm_eval.py
+++ vasm_eval.py
@@ -15,7 +15,64 @@ import subprocess
 # code
 
 errors = []
+COMMENT_CHAR = ';'
 
+ERROR_FAILED = 1
+ERROR_SUCCESS = 0
+ERROR_NOT_EQUAL = 2
+
+fnamed = '/tmp/vasm.asm' #TODO use a real temporary file
+
+def verify_equality(vasm, fname):
+    """Verify the equality of each couple.
+    Each line is supposed to be assembled correctly.
+    """
+    
+    previous_binary = previous_line = None
+    successes = []
+
+    f = open(fname)
+    for current_line in f.readlines():
+        # skip empty lines and comments
+        striped = current_line.strip()
+        if (not len(striped)) or striped[0] == COMMENT_CHAR:
+            continue
+    
+        # Create the file
+        d = open(fnamed, 'w') #TODO use temporary generated file
+        d.write(current_line)
+        d.close()
+
+        # launch line compilation
+        if not __assemble(vasm, fnamed, True, current_line[:-1]):
+            raise "WRONG test file" + fname
+
+        # Read the binary
+        with open('a.out', 'r') as a:
+            current_binary = a.read(-1)
+
+        # verify if we must compare
+        if previous_line is None:
+            previous_line = current_line
+            previous_binary = current_binary
+        else:
+            if previous_binary != current_binary:
+                sys.stdout.write('*')
+                repres = "%s => %s\nand\n%s => %s" % (previous_line.strip(), 
+                                                ",".join("0x%.2X"%ord(_) for _ in previous_binary),
+                                                current_line.strip(),
+                                                ",".join("0x%.2X"%ord(_) for _ in current_binary),
+                                               )
+                errors.append( (ERROR_NOT_EQUAL, fname, repres) )
+                successes.append(0)
+            else:
+                sys.stdout.write('o')
+                successes.append(1)
+            previous_binary = previous_line = None
+
+        
+    f.close()
+    return successes
 
 def assemble(vasm, fname, success=True):
     """Launch the assembling of the required file"""
@@ -26,7 +83,6 @@ def assemble(vasm, fname, success=True):
     else:
         successes = []
         f = open(fname)
-        fnamed = '/tmp/vasm.asm'
         for line in f.readlines():
             # skip empty lines
             if not len(line.strip()):
@@ -56,6 +112,8 @@ def __assemble(vasm, fname, success=True, content=None):
             errors.append( (success, content, stderr) )
         return 0
 
+
+
 def main():
     """Launch all the tests"""
     
@@ -68,6 +126,7 @@ def main():
 
     good_files = sorted(glob.glob('./good/*.asm'))
     bad_files = sorted(glob.glob('./bad/*.asm'))
+    equiv_files = sorted(glob.glob('./equiv/*.asm'))
 
     for fname in good_files:
         successes.extend(assemble(vasm, fname, True))
@@ -75,15 +134,21 @@ def main():
     for fname in bad_files:
         successes.extend(assemble(vasm, fname, False))
 
+    for fname in equiv_files:
+        successes.extend(verify_equality(vasm, fname))
+
     print
     print '%d/%d successes of tests.' % (sum(successes), len(successes))
     for error in errors:
-        print '\nSource:',error[1]
-        if error[0]:
-            print 'Failed instead of succeeded'
-            print 'Message:',error[2] 
+        print '\n>> Source:',error[1]
+        if error[0] == ERROR_FAILED:
+            print '\tFailed instead of succeeded'
+            print '\tMessage:',error[2] 
+        elif error[0] == ERROR_SUCCESS:
+            print '\tSucceeded instead of failed'
         else:
-            print 'Succeeded instead of failed'
+            print '\tInstruction assembled differently'
+            print '\tMessage:\n', error[2]
                                     
 
 if __name__ == '__main__':
