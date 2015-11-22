90d7df77da0f286286095e24304e3c59aecde5ce
Romain <giot.romain@gmail.com>
2015-11-22 15:21:39 +0100

    improve reading of high2 test

--- good/high2.asm
+++ good/high2.asm
@@ -10,9 +10,9 @@ PFO100_try2 equ >DICO100        ; must be 0x12
 PFO100_try3 equ 0x12            ; must be 0x12
 NBR_REG equ 13                  ; must be 13
 
-    CP PFO100_try1+NBR_REG      ; must be 0x12+13
-    CP PFO100_try2+NBR_REG      ; must be 0x12+13
-    CP PFO100_try3+NBR_REG      ; must be 0x12+13
-    CP 0x12+13                  ; must be 0x12+13
+    CP PFO100_try1+NBR_REG      ; must be 0x1f
+    CP PFO100_try2+NBR_REG      ; must be 0x1f
+    CP PFO100_try3+NBR_REG      ; must be 0x1f
+    CP 0x12+13                  ; must be 0x1f
 
 
