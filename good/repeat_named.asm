e7151b9ba382370e4c226993a7e449b48baaf9da
Romain Giot <romain.giot@u-bordeaux.fr>
2017-03-06 09:08:43 +0100

    Improve the named repetition

--- good/repeat_named.asm
+++ good/repeat_named.asm
@@ -1,6 +1,15 @@
 
 
 
+    ; standard repeat
     repeat 50 , "variable"
         db variable
+    endrepeat
+
+
+    ; Nested repeats
+    repeat 10, Y
+        repeat 20, X
+            db X, Y
+        endrepeat
     endrepeat
